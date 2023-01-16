829e5f8b3aa535bf622cd8bcb34fab7ba4b88a4a
Pop Laura Nicoleta <117296716+lauranicoleta@users.noreply.github.com>
2023-01-17 00:14:32 +0200

    Update Event.cpp

--- Event.cpp
+++ Event.cpp
@@ -1,18 +1,20 @@
 #include "Event.h"
 
-Event::Event() :idEvent(++nextIdEvent) 
+Event::Event() :idEvent(++nextIdEvent)
 {
 	nameOfEvent = nullptr;
 	dateOfEvent = "";
 };
-Event::Event(char* nameOfEvent, string dateOfEvent) : Event()
+Event::Event(char* nameOfEvent, string dateOfEvent, Locator lc) : Event()
 {
 	this->nameOfEvent = new char[strlen(nameOfEvent) + 1];
 	strcpy_s(this->nameOfEvent, strlen(nameOfEvent) + 1, nameOfEvent);
 
 	this->dateOfEvent = dateOfEvent;
+
+	this->lc = lc;
 };
-Event::Event(const Event& ev) : idEvent(ev.idEvent) 
+Event::Event(const Event& ev) : idEvent(ev.idEvent)
 {
 	if (ev.nameOfEvent != nullptr && strlen(ev.nameOfEvent) > 0)
 	{
@@ -21,6 +23,8 @@ Event::Event(const Event& ev) : idEvent(ev.idEvent)
 	}
 
 	this->dateOfEvent = ev.dateOfEvent;
+
+	this->lc = ev.lc;
 };
 Event::~Event()
 {
@@ -40,9 +44,14 @@ Event& Event::operator=(const Event& ev)
 		}
 
 		this->dateOfEvent = ev.dateOfEvent;
-
+		this->lc = ev.lc;
 		return *this;
 	}
+}
+
+string Event::getAdminPasscodeEvent()
+{
+	return adminPasscodeEvent;
 }
 
 void Event::setNameOfEvent(char* nameOfEvent)
@@ -81,7 +90,7 @@ bool Event::checkFutureDateOfEvent(string dateOfEvent)
 {
 	struct tm newtime;
 	time_t now = time(0);
-	localtime_s(&newtime, &now);
+	localtime_s(&newtime, &now); 
 
 	string year = dateOfEvent.substr(0, 4);
 	string month = dateOfEvent.substr(5, 2);
@@ -93,7 +102,10 @@ bool Event::checkFutureDateOfEvent(string dateOfEvent)
 
 	if (yearInt > (1900 + newtime.tm_year))
 	{
-		return true;
+		if (monthInt <= 12 && dayInt <= 31)
+			return true;
+		else
+			return false;
 	}
 	else if (yearInt < (1900 + newtime.tm_year))
 	{
@@ -106,21 +118,32 @@ bool Event::checkFutureDateOfEvent(string dateOfEvent)
 			if (monthInt > 1 + newtime.tm_mon)
 			{
 				//adding 1 to get the current month
-				return true;
+				if (monthInt <= 12)
+					return true;
+				else
+				{
+					cout << "Invalid month" << endl; return false;
+				}
 			}
 			else if (monthInt < 1 + newtime.tm_mon)
 			{
 				return false;
 			}
-			else if ((monthInt == 1 + newtime.tm_mon) && monthInt <= 12) {
+			else {
+				if ((monthInt == 1 + newtime.tm_mon)) {
 
-				if ((dayInt >= newtime.tm_mday) && dayInt <= 31)
-					return true;
-				else
-				{
-					cout << "Invalid day" << endl; return false;
+					if ((dayInt >= newtime.tm_mday))
+						if(dayInt <= 31)
+							return true;
+						else
+						{
+							cout << "Invalid day" << endl; return false;
+						}
+					else
+					{
+						cout << "Invalid day" << endl; return false;
+					}
 				}
-
 			}
 		}
 	}
@@ -132,13 +155,38 @@ bool Event::operator!()
 		return false;
 }
 
+Locator Event::getLocatorForThisEvent()
+{
+	return this->lc;
+}
+
+int Event::getIdOfEvent()
+{
+	return this->idEvent;
+}
+
+void Event::setLocatorForThisEvent(Locator lc)
+{
+	this->lc = lc;
+}
+
 
 ostream& operator<<(ostream& out, Event ev)
 {
 	out << "Id of Event: " << ev.idEvent << endl;
 	out << "Name of event: " << ev.nameOfEvent << endl;
 	out << "Date of event: " << ev.dateOfEvent << endl;
+	out << endl;
 	return out;
+}
+
+ofstream& operator<<(ofstream& f, Event ev)
+{
+	f << ev.idEvent << endl;
+	f << ev.nameOfEvent << endl;
+	f << ev.dateOfEvent << endl;
+	f << endl;
+	return f;
 }
 
 istream& operator>>(istream& in, Event& ev)
@@ -153,21 +201,37 @@ istream& operator>>(istream& in, Event& ev)
 		ev.nameOfEvent = new char[buffer.length() + 1];
 		strcpy_s(ev.nameOfEvent, buffer.length() + 1, buffer.c_str());
 	}
-	cout << "Enter the future date of event(YYYY.MM.DD): ";
+	cout << "Enter the future date of event: " << endl;
 	string bufferDate;
-	in >> bufferDate; 
+	string bY;
+	string bM;
+	string bD;
+	cout << "Day(DD): "; in >> bD; cout << endl;
+	cout << "Month(MM): "; in >> bM; cout << endl;
+	cout << "Year(YYYY): "; in >> bY; cout << endl;
+
+	while(bY.length() != 4 || bM.length() != 2 || bD.length() != 2)
+	{
+		cout << "Invalid date!" << endl;
+		cout << "Day(DD): "; in >> bD; cout << endl;
+		cout << "Month(MM): "; in >> bM; cout << endl;
+		cout << "Year(YYYY): "; in >> bY; cout << endl;
+	}
+	bufferDate = bY + "." + bM + "." + bD;
+	cout << bufferDate << endl << endl;
 	while (ev.checkFutureDateOfEvent(bufferDate) == false)
 	{
 		cout << "This date is invalid, you must enter a future date in order to add a future event." << endl;
 		cout << "Enter the future date of event(YYYY.MM.DD): ";
 		bufferDate = "";
-		in >> bufferDate;
+		cout << "Day(DD): "; in >> bD; cout << endl;
+		cout << "Month(MM): "; in >> bM; cout << endl;
+		cout << "Year(YYYY): "; in >> bY; cout << endl;
+		bufferDate = bY + "." + bM + "." + bD;
 		cout << endl;
-		cout << ev.checkFutureDateOfEvent(bufferDate) << endl;
 	}
+
 	ev.dateOfEvent = bufferDate;
-	cout << "For " << ev.nameOfEvent << " Event ";
-	in >> ev.lc;
 	return in;
 }
 
